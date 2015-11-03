359eea0965d63e1bce2d60fc1aefb6e196252c33
Jonathan Müller <j.mueller@apoveda.ch>
2015-11-03 17:15:41 +0100

    easier hash factor

--- gen_hash.py
+++ gen_hash.py
@@ -5,5 +5,5 @@ import sys
 
 print('Enter token:')
 passw = sys.stdin.readline().strip().encode('utf-8')
-hash = bcrypt.hashpw(passw, bcrypt.gensalt())
+hash = bcrypt.hashpw(passw, bcrypt.gensalt(6))
 print("slack_token = %s" % str(hash, 'ascii'))
