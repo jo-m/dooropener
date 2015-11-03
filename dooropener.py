8ce2de53424b480ab018d1268729d4312d5f7446
Jonathan Müller <j.mueller@apoveda.ch>
2015-11-03 16:58:00 +0100

    use post data instead of GET

--- dooropener.py
+++ dooropener.py
@@ -16,28 +16,30 @@ def do_open_door():
     os.system('sudo rpio --setoutput 18:1; sudo rpio --setoutput 18:0')
     print('opened the door')
 
-def log_door(username):
-    text = "User *%s* just opened the CWS door via _/dooropen_" % username
+def log_message(msg):
     payload = dict(
-        text=text,
+        text=msg,
         username='cwsdoor',
         icon_emoji=':cws:'
     )
     requests.post(slack_webhook, data=json.dumps(payload))
 
-@app.route('/dooropen/', methods=['POST', 'GET'])
+@app.route('/dooropen/', methods=['POST'])
 def dooropen():
     """
     Standard endpoint
     """
-    password = request.args.get('token', '').strip().encode('utf-8')
-    user_id = request.args.get('user_id', '').strip()
-    user_name = request.args.get('user_name', '').strip()
+    password = request.form['token'].strip().encode('utf-8')
+    user_id = request.form['user_id'].strip()
+    user_name = request.form['user_name'].strip()
 
     if not bcrypt.hashpw(password, slack_token) == slack_token:
         return 'Invalid token'
 
-    log_door('%s (%s)' % (user_name, user_id))
+    msg = "User *%s (%s)* just opened the CWS door via _/dooropen_" % \
+        (user_name, user_id)
+    log_message(msg)
+
     do_open_door()
 
     return 'Door is open'
@@ -48,4 +50,4 @@ slack_token = config['dooropener']['slack_token'].encode('utf-8')
 slack_webhook = config['dooropener']['slack_webhook'].strip('\'"')
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=5050, debug=True)
+    app.run(host='127.0.0.1', port=5050)
